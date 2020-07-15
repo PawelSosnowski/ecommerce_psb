89f8dec2f9a8332902324c0dbfae1b4179d6a5c6
Pawel Sosnowski <pawelsosn@gmail.com>
2020-07-15 15:50:53 +0200

    removed venv

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Pablito\Desktop\STUDIA\sklep internetowy - pbd\app\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
