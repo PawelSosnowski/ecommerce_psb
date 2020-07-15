9b6367e830275bf276f836e3131db9b00882378d
Pawel Sosnowski <pawelsosn@gmail.com>
2020-07-15 20:57:59 +0200

    tomerge

--- store/views.py
+++ store/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Producent
+from .models import Uzytkownik, Producent
+
 # Create your views here.
 
 
 def reports(request):
     lista_producentow = Producent.objects.order_by('idproducent')
     # output = ', '.join([p.nazwa for p in lista_producentow])
-    context = {}
-    return render(request, 'store/reports.html', context)
+    return render(request, 'store/reports.html')
+
 
 def generate_raports(request):
-    context = {}
+    lista_uzytkownikow = Uzytkownik.objects.raw('SELECT idUzytkownik, Imie, Nazwisko, Email, ifnull(LiczbaZamowien, 0 ) as LiczbaZamowien from v_rklienciliczba')
+    context = {"uzytkownicy": lista_uzytkownikow}
     return render(request, 'store/generate_report.html', context)
+
 
 def managements(request):
     context = {}
