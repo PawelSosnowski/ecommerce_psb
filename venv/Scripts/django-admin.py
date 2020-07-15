8fa6c37feb474767dad1ac427aaf79bcec4d75bc
Pawel Sosnowski <pawelsosn@gmail.com>
2020-07-15 14:02:59 +0200

    venv

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Pablito\Desktop\STUDIA\sklep internetowy - pbd\app\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
