2df509610519aa4f8aeef32352a39c3c4394a8d0
apiec <apieczul97@gmail.com>
2020-07-15 20:50:29 +0200

    Add sorttable.js. Change management.html.

--- store/views.py
+++ store/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Producent
+from .models import Produkt
+
+
 # Create your views here.
 
 
@@ -10,12 +13,30 @@ def reports(request):
     context = {}
     return render(request, 'store/reports.html', context)
 
+
 def generate_raports(request):
     context = {}
     return render(request, 'store/generate_report.html', context)
 
+
 def managements(request):
-    context = {}
+    query = '''
+    SELECT
+        produkt.idProdukt AS idProdukt,
+        produkt.Nazwa AS produktNazwa,
+        kategoria.Nazwa AS kategoriaNazwa,
+        producent.Nazwa AS producentNazwa,
+        produkt.IloscMagazynowa AS ilosc
+    FROM
+        produkt
+            JOIN
+        producent ON produkt.idProducentaProduktu = producent.idProducent
+            JOIN
+        kategoria ON produkt.idKategoriiProduktu = kategoria.idKategoria
+    WHERE
+        NOT produkt.CzyArchiwalny
+    '''
+    context = {'products': Produkt.objects.raw(query)}
     return render(request, 'store/management.html', context)
 
 
